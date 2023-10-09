251cc12c0d0be3f32f39ac8aed7d05968910dec0
capthehacker99 <pawat.capsule@gmail.com>
2023-10-09 13:03:56 +0700

    Actions?

--- java.old/net/fabricmc/example/interfaces/IVillagerEntity.java
+++
@@ -1,9 +0,0 @@
-package net.fabricmc.example.interfaces;
-
-import net.minecraft.village.VillagerProfession;
-
-public interface IVillagerEntity {
-    VillagerProfession getprevProfession();
-
-    void setprevProfession(VillagerProfession pp);
-}
