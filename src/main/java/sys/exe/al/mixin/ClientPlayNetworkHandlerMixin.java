1a64f2133c15a7edcaeb481a21c4f3c45740dd33
capthehacker99 <pawat.capsule@gmail.com>
2023-10-13 16:41:03 +0700

    Reorganize Trade Locking Code.

--- src/main/java/sys/exe/al/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/sys/exe/al/mixin/ClientPlayNetworkHandlerMixin.java
@@ -7,13 +7,13 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientCommonNetworkHandler;
 import net.minecraft.client.network.ClientConnectionState;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.CommandSource;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -121,102 +121,152 @@ public abstract class ClientPlayNetworkHandlerMixin extends ClientCommonNetworkH
     }
 
     @Unique
-    private int findCheapestTrade(final PlayerInventory inventory, final TradeOfferList offers) {
-        int availEmerald = 0;
-        int availBook = 0;
-        int availPaper = 0;
-        for(final var stack : inventory.main) {
-            if(stack.isEmpty())
-                continue;
-            final var itm = stack.getItem();
-            if(itm == Items.EMERALD) availEmerald += stack.getCount();
-            else if(itm == Items.BOOK) availBook += stack.getCount();
-            else if(itm == Items.PAPER) availPaper += stack.getCount();
-        }
+    private int checkTrades(final AutoLectern AL, final TradeOfferList offers, final ClientPlayerEntity plr) {
         int curIdx = 0;
-        int minEm = 65;
-        int minIdx = -1;
         for(final var offer : offers) {
-            final var first = offer.getAdjustedFirstBuyItem();
-            if(first.getItem() == Items.PAPER && first.getCount() <= availPaper)
-                return curIdx;
-            else if(first.getItem() == Items.EMERALD){
-                final var count = first.getCount();
-                if(count < minEm && count <= availEmerald) {
-                    final var secondItem = offer.getSecondBuyItem();
-                    if(secondItem.getItem() != Items.BOOK || secondItem.getCount() <= availBook) {
-                        minIdx = curIdx;
-                        minEm = count;
-                    }
-                }
-            }
-            ++curIdx;
-        }
-        return minIdx;
-    }
-
-    @Inject(method = "onSetTradeOffers", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V", shift = At.Shift.AFTER), cancellable = true)
-    private void onSetTradeOffers(final SetTradeOffersS2CPacket packet, final CallbackInfo ci) {
-        if(packet.getSyncId() != merchantSyncId) return;
-        final var AL = AutoLectern.getInstance();
-        final var plr = this.client.player;
-        if (AL.getState() != ALState.WAITING_TRADE || plr == null) {
-            sendPacket(new CloseHandledScreenC2SPacket(merchantSyncId));
-            return;
-        }
-        ++AL.attempts;
-        int curIdx = 0;
-        for(final var offer : packet.getOffers()) {
             final var sellItem = offer.getSellItem();
             if(sellItem.getItem() != Items.ENCHANTED_BOOK)
                 continue;
             for(final var entry : EnchantedBookItem.getEnchantmentNbt(sellItem)) {
-                final var compound = (NbtCompound)entry;
+                final var compound = (NbtCompound) entry;
                 final var opt = Registries.ENCHANTMENT.getOrEmpty(EnchantmentHelper.getIdFromNbt(compound));
-                if(opt.isEmpty())
+                if (opt.isEmpty())
                     continue;
                 final var enchant = opt.get();
                 final var lvl = EnchantmentHelper.getLevelFromNbt(compound);
-                if(AL.logTrade) {
+                if (AL.logTrade) {
                     plr.sendMessage(Text.literal("[Auto Lectern] ")
-                            .formatted(Formatting.YELLOW)
-                            .append(
-                                    Text.literal(String.valueOf(offer.getOriginalFirstBuyItem().getCount()))
-                                        .formatted(Formatting.GREEN).append(
-                                            Text.literal(" emeralds for ")
-                                                    .formatted(Formatting.WHITE)
-                                                    .append(enchant.getName(lvl)).append(Text.literal(" [" + AL.attempts + "]")
-                                                            .formatted(Formatting.WHITE)
-                                                    )
-                                            )
+                        .formatted(Formatting.YELLOW)
+                        .append(Text.literal(String.valueOf(offer.getOriginalFirstBuyItem().getCount()))
+                            .formatted(Formatting.GREEN).append(Text.literal(" emeralds for ")
+                                .formatted(Formatting.WHITE)
+                                .append(enchant.getName(lvl)).append(Text.literal(" [" + AL.attempts + "]")
+                                        .formatted(Formatting.WHITE)
+                                )
                             )
+                        )
                     );
                 }
-                if(AL.isGoalMet(offer.getOriginalFirstBuyItem().getCount(), enchant, lvl)) {
-                    if(AL.autoTrade != ALAutoTrade.OFF) {
-                        final var empty = plr.getInventory().getEmptySlot();
-                        if(empty == -1)
-                            break;
-                        final int selIdx;
-                        if(AL.autoTrade == ALAutoTrade.ENCHANT)
-                            selIdx = curIdx;
-                        else
-                            selIdx = findCheapestTrade(plr.getInventory(), packet.getOffers());
-                        sendPacket(new SelectMerchantTradeC2SPacket(selIdx));
-                        final var map = new Int2ObjectOpenHashMap<ItemStack>(0);
-                        sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, 2, 0, SlotActionType.PICKUP, sellItem, map));
-                        sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, empty, 0, SlotActionType.PICKUP, ItemStack.EMPTY, map));
-                    }
-                    sendPacket(new CloseHandledScreenC2SPacket(merchantSyncId));
-                    AL.signal(AutoLectern.SIGNAL_TRADE | AutoLectern.SIGNAL_TRADE_OK);
-                    ci.cancel();
-                    return;
-                }
+                if (AL.isGoalMet(offer.getOriginalFirstBuyItem().getCount(), enchant, lvl))
+                    return curIdx;
             }
             ++curIdx;
         }
+        return -1;
+    }
+
+    @Unique
+    private void attemptLockTrade(final AutoLectern AL, final TradeOfferList offers, final ClientPlayerEntity plr, int tarIdx) {
+        final var emptySlot = plr.getInventory().getEmptySlot();
+        if (emptySlot == -1)
+            return;
+        if(AL.autoTrade == ALAutoTrade.ENCHANT) {
+            int availEmerald = 0;
+            int availBook = 0;
+            for (final var stack : plr.getInventory().main) {
+                if (stack.isEmpty())
+                    continue;
+                final var itm = stack.getItem();
+                if (itm == Items.EMERALD) availEmerald += stack.getCount();
+                else if (itm == Items.BOOK) availBook += stack.getCount();
+            }
+            final var tarOffer = offers.get(tarIdx);
+            final var first = tarOffer.getAdjustedFirstBuyItem();
+            if(first.getItem() != Items.EMERALD)
+                return;
+            final var second = tarOffer.getSecondBuyItem();
+            if(second.getItem() != Items.BOOK)
+                return;
+            if(first.getCount() > availEmerald || second.getCount() > availBook)
+                return;
+            sendPacket(new SelectMerchantTradeC2SPacket(tarIdx));
+            final var map = new Int2ObjectOpenHashMap<ItemStack>(0);
+            sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, 2, 0, SlotActionType.PICKUP, tarOffer.getSellItem(), map));
+            sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, emptySlot, 0, SlotActionType.PICKUP, ItemStack.EMPTY, map));
+        } else {
+            final var minIdx = findCheapestTrade(offers, plr);
+            if(minIdx == -1)
+                return;
+            sendPacket(new SelectMerchantTradeC2SPacket(minIdx));
+            final var map = new Int2ObjectOpenHashMap<ItemStack>(0);
+            sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, 2, 0, SlotActionType.PICKUP, offers.get(minIdx).getSellItem(), map));
+            sendPacket(new ClickSlotC2SPacket(merchantSyncId, 6, emptySlot, 0, SlotActionType.PICKUP, ItemStack.EMPTY, map));
+        }
+
+    }
+
+    @Unique
+    private static int findCheapestTrade(final TradeOfferList offers, final ClientPlayerEntity plr) {
+        int availEmerald = 0;
+        int availBook = 0;
+        int availPaper = 0;
+        for (final var stack : plr.getInventory().main) {
+            if (stack.isEmpty())
+                continue;
+            final var itm = stack.getItem();
+            if (itm == Items.EMERALD) availEmerald += stack.getCount();
+            else if (itm == Items.BOOK) availBook += stack.getCount();
+            else if (itm == Items.PAPER) availPaper += stack.getCount();
+        }
+        int minIdx = -1;
+        float minCost = Float.MAX_VALUE;
+        int curIdx = minIdx;
+        for(final var offer : offers) {
+            ++curIdx;
+            float curCost = 0;
+            final var first = offer.getAdjustedFirstBuyItem();
+            final var firstItem = first.getItem();
+            if(firstItem == Items.EMERALD) {
+                final var count = first.getCount();
+                if (count > availEmerald) {
+                    continue;
+                }
+                curCost += count;
+            } else if(firstItem == Items.PAPER) {
+                final var count = first.getCount();
+                if (first.getCount() > availPaper) {
+                    continue;
+                }
+                curCost += count * 0.015625f;
+            } else if(firstItem == Items.BOOK) {
+                final var count = first.getCount();
+                if (first.getCount() > availBook) {
+                    continue;
+                }
+                curCost += count * 0.146875f;
+            } else
+                continue;
+            if(curCost < minCost) {
+                minIdx = curIdx;
+                minCost = curCost;
+            }
+        }
+        return minIdx;
+    }
+
+    @Unique
+    private void determineTrades(final TradeOfferList offers) {
+        final var AL = AutoLectern.getInstance();
+        final ClientPlayerEntity plr;
+        if (AL.getState() != ALState.WAITING_TRADE || (plr = this.client.player) == null)
+            return;
+        ++AL.attempts;
+        int tarIdx = checkTrades(AL, offers, plr);
+        if(tarIdx == -1) {
+            AL.signal(AutoLectern.SIGNAL_TRADE);
+            return;
+        }
+        AL.signal(AutoLectern.SIGNAL_TRADE | AutoLectern.SIGNAL_TRADE_OK);
+        if(AL.autoTrade != ALAutoTrade.OFF)
+            attemptLockTrade(AL, offers, plr, tarIdx);
+    }
+
+
+    @Inject(method = "onSetTradeOffers", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V", shift = At.Shift.AFTER), cancellable = true)
+    private void onSetTradeOffers(final SetTradeOffersS2CPacket packet, final CallbackInfo ci) {
+        if(packet.getSyncId() != merchantSyncId) return;
+        determineTrades(packet.getOffers());
         sendPacket(new CloseHandledScreenC2SPacket(merchantSyncId));
-        AL.signal(AutoLectern.SIGNAL_TRADE);
         ci.cancel();
     }
 
